b86afdc5c7da11d1deffec593f19857eb360a196
Rina Estrina <est.rishka@gmail.com>
2025-06-27 09:36:31 +0300

    registration, firebase

--- src/components/servicrPages/Registration.tsx
+++ src/components/servicrPages/Registration.tsx
@@ -1,10 +1,25 @@
 import SignUpForm from "../templates/SignUpForm.tsx";
-import type {SignupData} from "../../utils/shop-types.ts";
+import type {LoginData, SignupData} from "../../utils/shop-types.ts";
+import {registerWithEmailAndPassword} from "../../firebase/firebaseAuthService.ts";
+import {useNavigate} from "react-router-dom";
 
 
 const Registration = () => {
+    const navigate = useNavigate();
     const signUpSubmit = (data:SignupData) => {
         console.log(JSON.stringify(data))
+    }
+    const signUpWithEmail = async (data: SignupData) => {
+        const userEmailPass:LoginData = {
+            email:data.email,
+            password:data.password
+        }
+        try {
+            await registerWithEmailAndPassword(userEmailPass)
+        navigate('/login')
+        } catch (e){
+            console.error(e);
+        }
     }
     return (
         <div>
--- src/firebase/firebaseAuthService.ts
+++ src/firebase/firebaseAuthService.ts
@@ -11,8 +11,18 @@ const loginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
-    return Promise.resolve("")
+    console.log(auth.currentUser)
+    return Promise.resolve(user.email)
 }
 export const login = async (data: LoginData) => {
     return data.email === "GOOGLE" ? loginWithGoogle() : loginWithEmail(data)
+}
+
+export const registerWithEmailAndPassword = async (data:LoginData)=> {
+    await createUserWithEmailAndPassword(auth, data.email, data.password)
+    return data.email;
+}
+
+export const exit = async () => {
+    await signOut(auth);
 }
